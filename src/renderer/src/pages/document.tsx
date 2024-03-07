395d2013c4b35d8d8c44efa204c6755ce642b933
Ash CPC <jv.palha@gmail.com>
2024-03-07 17:21:22 -0300

    add document page

--- src/renderer/src/pages/document.tsx
+++ src/renderer/src/pages/document.tsx
@@ -1,10 +1,24 @@
 import { Link } from "react-router-dom";
-
+import {ToC} from "../components/ToC"
 export function Document(){
     return (
-        <>
-            <h1>Document</h1>
-            <Link to="/" className="text-rotion-100">Voltar</Link>
-        </>
+        <main className="flex-1 flex py-12 px-10 gap-8">
+            <aside className="hidden lg:block sticky top-0">
+                <span className="text-rotion-300 font-semibold text-xs">
+                    TABLE OF CONTENTS
+                </span>
+                <ToC.Root>
+                    <ToC.Link>Back-end</ToC.Link>
+                    <ToC.Section>
+                        <ToC.Link>Back-end</ToC.Link>
+                        <ToC.Link>Back-end</ToC.Link>
+                    </ToC.Section>
+                </ToC.Root>
+            </aside>
+
+            <section className="flex-1 flex flex-col items-center">
+                
+            </section>
+        </main>
     )
 }
